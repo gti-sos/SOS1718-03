bd4dfc289a2a466dcfbfc6001976a34d85c0e7a0
sos171803jmbo <pema_12@hotmail.com>
2018-03-05 13:19:00 +0000

    servidor para todo el grupo

---
+++ index.js
@@ -0,0 +1,10 @@
+//index of hellojm
+
+var express = require("express");
+var app = express();
+
+app.get("/hello",(req,res)=>{
+    res.send("hello world!");
+});
+
+app.listen(process.env.PORT);
--- l04/index.js
+++
@@ -1,9 +0,0 @@
-var express =  require("express");
-var app = express();
-app.get("/hello",(req,res)=>{
-    
-    res.send("hello!!");
-});
-
-app.listen(process.env.PORT);
-console.log("Hello world!");
