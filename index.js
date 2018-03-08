8b22fffb00a0f887af3cb4f819f852b9cac9fa78
sos171803ajpg <asir2011pegua@gmail.com>
2018-03-08 20:11:52 +0000

    index

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+ //index lab04
+    // var express = require("express");
+    //var app = express();
+    //app.get("/hello",(req,res)=>{
+    //   res.send("hello world!");
+    //});
+    //app.listen(process.env.PORT);
+
 //index Lab05 
 
 var express = require("express");
-var app = express();
 var port = (process.env.PORT || 1607);
+
 var app = express();
 
+app.use("/",express.static("/home/ubuntu/workspace/SOS1718-03/sos1718-03/public"));
+
+
+app.get("/time",(req,res)=>{
+    console.log("new request to /time");
+    res.send(new Date());
+    
+});
    
-    //lab04
-    //app.get("/hello",(req,res)=>{
-    //   res.send("hello world!");
-app.listen(port);
+   
+app.listen(port,()=>{
+    
+    console.log(" Server ready on port "+port+"!");
+    
+}).on("error", (e)=>{
+    console.log("Server NOT READY:"+e);
+});
+
+console.log("Server setting up...");
