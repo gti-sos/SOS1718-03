cea213daf9f0eb1a5b16f1db8af031517ca758a2
sos171803amfm <adan_fm1@hotmail.com>
2018-03-15 22:11:05 +0000

    adan

--- index.js
+++ index.js
@@ -1,7 +1,143 @@
- //index lab04
-    var express = require("express");
-    var app = express();
-    app.get("/hello",(req,res)=>{
-       res.send("hello world!");
+//IndexLab06
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+var port = (process.env.PORT || 1607);
+var BASE_API_PATH = "/api/v1";
+
+var app = express();
+app.use("/",express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+
+var cities = [
+    
+     {"city"  :  "madrid",
+    "station" :"fernandez-ladreda-oporto",
+    "year" : "2014"
+    },
+    
+     {"city"  :  "barcelona",
+    "station" :"l-eixample",
+    "year" : "2014"
+    },
+    
+     {"city"  :  "barcelona",
+    "station" :"gracia-sant-gervasi",
+    "year" : "2014"
+    },
+     
+     {"city"  :  "madrid",
+    "station" :"escuelas-aguirre",
+    "year" : "2014"
+    },
+     
+     {"city"  :  "valencia",
+    "station" :"pista-de-silla",
+    "year" : "2014"
+    }
+   
+    ];
+    
+    
+//--------------------------------------------------------------------------------    
+    
+    
+//GET al conjunto de recursos    
+app.get(BASE_API_PATH+"/cities",(req,res)=>{
+    console.log(Date() + " - GET /cities");
+    res.send(cities);
+});
+
+
+
+//GET a un recurso concreto /station
+app.get(BASE_API_PATH+"/cities/:station",(req,res)=>{
+     
+    var station = req.params.station;
+     console.log(Date() + " - GET /cities/"+ station);
+    var filteredCities = cities.filter((c)=>{
+        return (c.station == station);
     });
-    app.listen(process.env.PORT);
+
+   res.send(filteredCities);
+});
+
+// POST al conjunto de recursos   
+app.post(BASE_API_PATH+"/cities",(req,res)=>{ 
+    console.log(Date() + " - POST /cities");
+    var city = req.body;
+    cities.push(city);
+     
+    res.sendStatus(201);
+});
+
+//POST a un recurso
+app.post(BASE_API_PATH + "/cities/:station",(req,res)=>{
+    var station = req.params.station;
+     console.log(Date() + " - GET /cities/"+ station);
+    res.sendStatus(405);
+}); 
+
+//--------------------------------------------------------------------------------
+
+
+//DELETE a un conjunto recursos
+app.delete(BASE_API_PATH+"/cities",(req,res)=>{
+    console.log(Date() + " - DELETE /cities");
+    cities = [];
+    res.sendStatus(200);
+ });
+
+
+//DELETE a un recurso concreto
+app.delete(BASE_API_PATH+"/cities/:station",(req,res)=>{
+    var station = req.params.station;
+    console.log(Date() + " - DELETE /cities/"+ station);
+    cities = cities.filter((c)=>{
+        return c.station != station;
+    });
+   res.sendStatus(200);
+}); 
+
+//--------------------------------------------------------------------------------
+
+
+//PUT a un conjunto
+app.put(BASE_API_PATH+"/cities",(req,res)=>{
+    console.log(Date() + " - PUT /cities");
+    res.sendStatus(405);
+});  
+
+//PUT a un recurso concreto
+app.put(BASE_API_PATH+"/cities/:station",(req,res)=>{
+    var station = req.params.station;
+    var updateCities = req.body;
+    
+    console.log(Date() + " - PUT /cities/"+station);
+    
+    if(station != updateCities.station){
+        res.sendStatus(409);
+        return;
+    }
+   
+   cities = cities.map((c)=>{
+       if (c.station == updateCities.station){
+           return updateCities;
+       }else{
+           return c;
+       }
+   });
+    res.sendStatus(200);
+});
+
+ 
+   
+app.listen(port,()=>{
+    
+    console.log(" Server ready on port "+port+"!");
+    
+}).on("error", (e)=>{
+    console.log("Server NOT READY:"+e);
+});
