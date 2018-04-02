1f6d9e5456fe334a5511244140de712aaea39ab3
sos171803ajpg <asir2011pegua@gmail.com>
2018-04-02 09:00:47 +0000

    actualizacion

--- globlalWarmingsApi/index.js
+++ globlalWarmingsApi/index.js
@@ -65,7 +65,7 @@ app.get(BASE_API_PATH+"/global-warmings",(req,res)=>{
 
 app.get(BASE_API_PATH + "/global-warmings/docs", (req, res) => {
 
-    res.status(301).redirect("https://documenter.getpostman.com/view/4029210/sos1718-03-globalwarmings/RVtynWMu");
+    res.status(301).redirect("https://documenter.getpostman.com/view/4029210/sos1718-03-globalwarmings-heroku/RVu1HWcv");
 
 });
 
