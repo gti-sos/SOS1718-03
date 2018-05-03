fcf571be43898cad7d8eddb93c6446eb1ef8aa4d
sos171803ajpg <asir2011pegua@gmail.com>
2018-05-03 18:55:10 +0000

    bb

--- public/global/listCtrl.js
+++ public/global/listCtrl.js
@@ -1,6 +1,14 @@
  /* global angular */
+ /* global Highcharts */
+  /* global google */
+  /* global c3 */
  angular.module("globalApp").controller("ListCtrl", ["$scope", "$http", function($scope, $http) {
   console.log("List Ctrl initialited");
+  
+  
+
+  
+  console.log(2);
   var api = "/api/v2/global-warmings";
   var search = "?";
   var offset = -10;
@@ -110,6 +118,98 @@ function getGlobalWarmingsCount(){
       });
       search = "?";
    }
+
+
+  $http
+   .get("/api/v2/global-warmings")
+   .then(function(response){
+    console.log(response.data);
+     Highcharts.chart('container', {
+
+    title: {
+        text: 'SOLAR PLANT DATA'
+        
+    },
+
+    subtitle: {
+        text: 'Datas'
+    },
+
+     xAxis: {
+     
+     categories : response.data.map(function(d) {
+         return d.year;
+     })
+     
+    },
+    
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: true
+            },
+        }
+    },
+
+    series: [{
+        name: 'peakPower',
+        
+        data: response.data.map(function(d) {return parseInt(d.peakPower)})
+    },{
+        name: 'Temperature',
+        data: response.data.map(function(d) {return parseInt(d.temperature)})
+    }
+    
+    ]
+
+});
+    
+});
+  
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawSeriesChart);
+
+    function drawSeriesChart() {
+
+      var data = google.visualization.arrayToDataTable([
+       ['City', 'peakPower','temperature', 'solarPlant','year'],
+          ['Ciudad Real', 70,0.7,'Parque fotovoltaico puertollano',2010],
+          ['Cuenca', 60,0.7,'Parque fotovoltaico Olmedilla de Alarcon',2010],
+          ['Caceres', 34,0.7,'Planta solar fotovoltaica La Magascona y La Magasquilla',2010],
+          ['la Rioja', 30, 0.7, 'Planta solar Arnedo',2010],
+          ['Cuenca', 30, 0.7, 'Planta solar Osa de la Vega',2010],
+          ['Sevilla', 10, 1.5,'Sevilla Plant',2015]
+      ]);
+
+      var options = {
+        title: 'Correlation between peakPower, temperature rate ' +
+               'and solarPlant of some Spanish countries (2010-2015)',
+        hAxis: {title: 'peakPower'},
+        vAxis: {title: 'Temperature'},
+        bubble: {textStyle: {fontSize: 11}}
+      };
+
+      var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
+      chart.draw(data, options);
+    }
+
+var chart = c3.generate({
+ 
+    bindto: '#chart',
+    
+    data: {
+      columns: [
+        ['peakPower', 70, 60, 34, 30, 30, 10],
+        ['temperature',0.7,0.7,0.7,0.7,0.7,1.5]
+      ]
+    }
+});
 
 
  }]);
