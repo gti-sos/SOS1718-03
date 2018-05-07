90eaea84da67b980086dd9ca63c9d4376a66119a
sos171803ajpg <asir2011pegua@gmail.com>
2018-05-07 14:08:20 +0000

    f

--- public/pollution.1/graph2.js
+++ public/pollution.1/graph2.js
@@ -97,9 +97,12 @@ console.log(googleData);
     
     ]
     });
-          google.charts.load('current', {
-        'packages':['geochart']
-         });
+        google.charts.load('current', {
+        'packages':['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+      });
          
       google.charts.setOnLoadCallback(drawRegionsMap);
    
