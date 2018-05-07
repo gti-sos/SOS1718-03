65709b68e055f55d72b6b0a1757dcd31ca46653e
sos171803ajpg <asir2011pegua@gmail.com>
2018-05-07 11:22:20 +0000

    f

--- public/pollution.1/graph2.js
+++ public/pollution.1/graph2.js
@@ -102,7 +102,25 @@ console.log(googleData);
     
     ]
     });
+          google.charts.load('current', {
+        'packages':['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
 
+      function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable(googleData);
+
+        var options = {hAxis: { title: 'peakPower' },
+    vAxis: { title: 'Temperature'}};
+
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+        chart.draw(data, options);
+      }
+/*
   google.charts.load('current', { 'packages': ['corechart'] });
   google.charts.setOnLoadCallback(drawSeriesChart);
 
@@ -119,7 +137,7 @@ console.log(googleData);
    var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
    chart.draw(data, options);
   }
-
+*/
   var chart = c3.generate({
     data: {
         columns: [
