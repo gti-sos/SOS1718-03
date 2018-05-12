11fa2998f37f4452327e40b4039f78656b929ca2
sos171803amfm <adan_fm1@hotmail.com>
2018-05-12 11:19:54 +0000

    a

--- public/pollution.1/compartidaCtrlG.js
+++ public/pollution.1/compartidaCtrlG.js
@@ -7,7 +7,7 @@ angular.module("StatsApp")
         function($scope, $http) {
             console.log("List Ctrl initialized!");
             var apiPropia = "/api/v2/pollution-cities";
-           // var api2 = "proxyADAN/api/v2/pollution-cities";
+            var api2 = "proxyDAP/api/v1/divorces-an";
             var api = "https://sos171803ajpg-sandbox-sos171803ajpg.c9users.io/api/v1/global-warmings";
             
             
@@ -60,7 +60,71 @@ angular.module("StatsApp")
                         name: 'peakPower',
                         data: response1.data.map(function(d){return parseInt(d.peakPower)})
             }]
-});   
+});  
+
+
+
+
+
+
+                                        $http.get(api2).then(function(response1) {
+                                            $http.get(apiPropia).then(function(response2) {
+                                                Highcharts.chart('GraficoProxy', {
+                                                    chart: {
+                                                        type: 'bar'
+                                                    },
+                                                    title: {
+                                                        text: 'GraficoProxy'
+                                                    },
+
+                                                    xAxis: {
+                                                        categories: response2.data.map(function(d) { return (parseInt(d.year)) }),
+                                                        title: {
+                                                            text: null
+                                                        }
+                                                    },
+                                                    yAxis: {
+                                                        min: 0,
+                                                        title: {
+
+                                                            align: 'high'
+                                                        },
+                                                        labels: {
+                                                            overflow: 'justify'
+                                                        }
+                                                    },
+
+                                                    plotOptions: {
+                                                        bar: {
+                                                            dataLabels: {
+                                                                enabled: true
+                                                            }
+                                                        }
+                                                    },
+                                                    legend: {
+                                                        layout: 'vertical',
+                                                        align: 'right',
+                                                        verticalAlign: 'top',
+                                                        x: -40,
+                                                        y: 80,
+                                                        floating: true,
+                                                        borderWidth: 1,
+                                                        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                                                        shadow: true
+                                                    },
+                                                    credits: {
+                                                        enabled: false
+                                                    },
+                                                    series: [{
+                                                        name: 'car',
+                                                        data: response2.data.map(function(d) { return parseInt(d.car) })
+                                                    }, {
+                                                        name: 'break',
+                                                        data: response1.data.map(function(d) { return parseInt(d.break) })
+                                                    }]
+                                                });
+                                            });
+                                        });
 });
 });      
 }]);
