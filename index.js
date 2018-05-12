d9cdbd42e8a8aa98e16b9b9c2d02f1b2c7b51d90
sos171803amfm <adan_fm1@hotmail.com>
2018-05-12 10:54:40 +0000

    integracion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var path = require("path");
-var cors = require("cors");
+
 
 
 var MongoClient = require("mongodb").MongoClient;
--- public/pollution.1/compartidaCtrlG.js
+++ public/pollution.1/compartidaCtrlG.js
@@ -8,56 +8,59 @@ angular.module("StatsApp")
             console.log("List Ctrl initialized!");
             var apiPropia = "/api/v2/pollution-cities";
            // var api2 = "proxyADAN/api/v2/pollution-cities";
-            var api = "https://sos171803ajpg-sandbox-sos171803ajpg.c9users.io/api/v2/global-warmings";
+            var api = "https://sos171803ajpg-sandbox-sos171803ajpg.c9users.io/api/v1/global-warmings";
             
             
         $http.get(api).then(function(response1){
             $http.get(apiPropia).then(function(response2){
-                Highcharts.chart('GraficoNormal', {
-                    chart: {
-                        type: 'line'
-                    },
-                    title: {
-                        text: ' GraficoNormal '
-                    },
-                    
-                    xAxis: {
-                        categories: response2.data.map(function(d){return (parseInt(d.year))})
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Stats1'
-                        },
-                        labels: {
-                            formatter: function () {
-                                return this.value ;
-                            }
-                        }
-                    },
-                    tooltip: {
-                        crosshairs: true,
-                        shared: true
-                    },
-                    plotOptions: {
-                        spline: {
-                            marker: {
-                                radius: 4,
-                                lineColor: '#666666',
-                                lineWidth: 1
-                            }
-                        }
-                    },
-                    series: 
-                    [{
+     
+     Highcharts.chart('GraficoNormal', {
+    chart: {
+        type: 'area'
+    },
+    title: {
+        text: 'GraficoNormal'
+    },
+    xAxis: {
+        categories: response2.data.map(function(d){return (parseInt(d.year))}),
+        tickmarkPlacement: 'on',
+        title: {
+            enabled: false
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Stats'
+        },
+        labels: {
+            formatter: function () {
+                return this.value / 1000;
+            }
+        }
+    },
+    tooltip: {
+        split: true,
+        valueSuffix: 'und'
+    },
+    plotOptions: {
+        area: {
+            stacking: 'normal',
+            lineColor: '#666666',
+            lineWidth: 1,
+            marker: {
+                lineWidth: 1,
+                lineColor: '#666666'
+            }
+        }
+    },
+    series: [{
                         name: 'nitrous',
                         data: response2.data.map(function(d){return parseInt(d.nitrous)})
                         },{
-                        name: 'temperature',
-                        data: response1.data.map(function(d){return parseInt(d.temperature)})
-                    }]
+                        name: 'peakPower',
+                        data: response1.data.map(function(d){return parseInt(d.peakPower)})
+            }]
+});   
 });
-});
-});
-        
-       
+});      
 }]);
