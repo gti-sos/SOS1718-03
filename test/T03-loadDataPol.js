eee8097ad1b09024638a871b9b26ac7f7ba2d57f
sos171803amfm <adan_fm1@hotmail.com>
2018-05-23 21:54:14 +0000

    test4

--- test/T03-loadDataPol.js
+++ test/T03-loadDataPol.js
@@ -10,7 +10,7 @@ var config= require("./config");
 describe('Data is loaded', function() {
     
     it('should show some stations', function() {
-        browser.get(config.getAppUrl()).then(function() {
+        browser.get(config.getAppUrl()+"/#!/pollution").then(function() {
                 element.all(by.repeater('st in stations')).then(function(stations) {
                        browser.takeScreenshot().then(function (png){
                             
