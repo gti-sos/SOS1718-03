54fc663c9953a8ef6ba1c727d41ad06f37433f3c
sos171803amfm <adan_fm1@hotmail.com>
2018-05-23 20:44:46 +0000

    test3

--- test/T03-loadDataPol.js
+++ test/T03-loadDataPol.js
@@ -9,13 +9,13 @@ describe('Data is loaded', function() {
     it('should show some stations', function() {
         browser.get(config.getAppUrl()).then(function() {
                 element.all(by.repeater('st in stations')).then(function(stations) {
-                        browser.takeScreenshot().then(function (png){
+                       /* browser.takeScreenshot().then(function (png){
                             
                             var stream = fs.createWriteStream(path.join(process.cwd(),'test','output','T03.png'));
                             stream.write(new Buffer(png, 'base64'));
                             stream.end();
                             });
-                    
+                    */
                     expect(stations.length).toBeGreaterThan(8);
                 });
         });
