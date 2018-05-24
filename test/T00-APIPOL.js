675aabcda505643f12bc5b73433ab3665b7af7b0
sos171803amfm <adan_fm1@hotmail.com>
2018-05-24 02:28:13 +0000

    be

---
+++ test/T00-APIPOL.js
@@ -0,0 +1,20 @@
+var newman = require("newman");
+var path = require("path");
+
+
+
+describe('API works', function() {
+     newman.run({
+         collection: require(path.join(process.cwd(),"test","SOS1718-03-PruebasNewManPollution.postman_collection.json")),
+         reporters: "cli"
+     },function(err){
+         if(err)
+            throw new err;
+         else
+            console.log("Collection run complete!");
+
+     });
+});
+
+
+
