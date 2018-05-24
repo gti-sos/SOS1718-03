3b71c588b19012ca19e1d384efb91ae081b3320c
sos171803ajpg <asir2011pegua@gmail.com>
2018-05-24 01:09:22 +0000

    backend

--- test/config.js
+++ test/config.js
@@ -2,8 +2,8 @@ exports.config = {
     
     seleniumAddress: 'http://localhost:8910',
     
-   // specs: [],
-    specs: ['T03-loadDataPol.js','T04-AddPollution.js','T01-loadData.js','T02-AddGlobal.js'],
+   // specs: ['T00-APIGLOBAL.js','T03-loadDataPol.js','T04-AddPollution.js','T01-loadData.js','T02-AddGlobal.js'],
+    specs: ['T00-APIGLOBAL.js','T01-loadData.js','T02-AddGlobal.js','T03-loadDataPol.js','T04-AddPollution.js'],
     
     capabilities: {
         'browserName' : 'phantomjs'
