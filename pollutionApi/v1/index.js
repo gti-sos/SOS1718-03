8eca0234abce3df054341722d93d659895f3bad4
sos171803amfm <adan_fm1@hotmail.com>
2018-06-01 18:22:09 +0000

    a

--- pollutionApi/v1/index.js
+++ pollutionApi/v1/index.js
@@ -135,14 +135,14 @@ pollutionApi.register = function(app, db) {
                     aux2 = filteredCities.slice(offset, offset + limit);
                     res.send(aux2[0]);
                     }else{
-                    res.send("Not found");
+                    res.send([]);
                     }
                 }else{
                      if (filteredCities.length > 0) {
                     aux2 = filteredCities.slice(offset, offset + limit);
                     res.send(aux2);
                     }else{
-                    res.send("Not found");
+                    res.send([]);
                     }
                 }
                 
@@ -188,13 +188,13 @@ pollutionApi.register = function(app, db) {
                     if (filteredCities.length > 0) {
                         res.send(filteredCities[0]);
                     }else{
-                        res.send("Not found");
+                        res.send([]);
                     }
                 }else{
                     if (filteredCities.length > 0) {
                         res.send(filteredCities);
                     }else{
-                        res.send("Not found");
+                        res.send([]);
                     }
                 } 
                 
